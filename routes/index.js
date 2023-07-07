dd900158951494628682df82cd5103743b945c23
Piyush Bhatnagar <113785116+PiyushBhatnagar09@users.noreply.github.com>
2023-07-08 03:20:52 +0530

    add

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express= require('express');
 const router= express.Router();
 
@@ -8,20 +7,4 @@ const users_controller= require('../controllers/user_controller');
 router.use('/users', require('./users'));
 router.use('/registration', require('./registration'));
 router.get('/', users_controller.home);
-=======
-const express= require('express');
-const router= express.Router();
-const passport= require('passport');
-
-const home_controller= require('../controllers/home_controller');
-
-//made separate for handling user specific requests
-router.use('/users', require('./users'));
-router.use('/students', require('./students'));
-router.use('/interviews', require('./interviews'));
-router.use('/companies', require('./companies'));
-
-router.get('/', passport.checkAuthentication, home_controller.home);
-
->>>>>>> dee357abdf8dab5134a2b8951511bd7c735eb746
 module.exports= router;

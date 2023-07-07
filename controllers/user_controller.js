c4c86321792874442b08cc440bd8b71f89ac1356
Piyush Bhatnagar <113785116+PiyushBhatnagar09@users.noreply.github.com>
2023-07-08 03:28:53 +0530

    Delete controllers directory

--- controllers/user_controller.js
+++
@@ -1,9 +0,0 @@
-module.exports.home= function(req, res) {
-    return res.render('home');
-}
-module.exports.createAbha= function(req, res) {
-    return res.render('verifyAadhaar');
-}
-module.exports.loginAbha= function(req, res) {
-    return res.render('loginAbha');
-}
